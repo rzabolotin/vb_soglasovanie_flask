38f0e490d7cfceb72291e8a065530c53a48a02dd
rzabolotin <rzabolotin@gmail.com>
2020-06-17 10:35:53 +1000

    doing API post_task

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -22,22 +22,8 @@ def get_task(task_id:str):
 
 @blueprint.route('/post_task', methods=['POST'])
 def post_task():
-       
-    data_decode = request.data.decode('utf8')
-
-    data_json = json.loads(data_decode)
-
-    current_app.logger.info(data_json)
-
-    return data_json
-    
-    
     """
-    
-    if not request.json or not 'task_id' in request.json:
-        abort(400)
-    
-    sample_data = {
+     sample_input = {
         'task_id': '000000123',
         'bp_id': 'ВБ0000011',
         'bp_type': 'ТД_ЗаявкаНаОплату',
@@ -45,31 +31,42 @@ def post_task():
         'bp_decription': 'Заявка на оплату ... Контрагент: Заболотин ... Назначение платежа ...',
         'user': 'Перехожук'
     }
+    """
 
-    task = SoglasovanieTask.query.filter(SoglasovanieTask.task_id == request.json.task_id)
+
+    data_decode = request.data.decode('utf8')
+    try:
+        data_json = json.loads(data_decode)
+    except json.JSONDecodeError:
+        abort(400)
+
+    if not data_json or not 'task_id' in data_json:
+        abort(400)
+    
+    task = SoglasovanieTask.query.filter(SoglasovanieTask.task_id == data_json.task_id)
     
     if task:
         abort(400)
 
-    user = User.query.filter(User.username == request.json.user).first()
+    user = User.query.filter(User.username == data_json.user).first()
 
     if not user:
-        user = User(username=request.json.user)
+        user = User(username=data_json.user)
         db.session.add(user)
 
-    bp = BusinessProcess.query.filter(BusinessProcess.bp_id == request.json.bp_id).first()
+    bp = BusinessProcess.query.filter(BusinessProcess.bp_id == data_json.bp_id).first()
 
     if not bp:
         bp = BusinessProcess(
-            bp_id = request.json.bp_id,
-            bp_type = request.json.bp_type,
-            title = request.json.bp_title,
-            description = request.json.bp_description
+            bp_id = data_json.bp_id,
+            bp_type = data_json.bp_type,
+            title = data_json.bp_title,
+            description = data_json.bp_description
         )
         db.session.add(bp)
 
     task = SoglasovanieTask(
-        task_id = request.json.task_id,
+        task_id = data_json.task_id,
         bp_id = bp.bp_id,
         user_id = user.id,
     ) 
@@ -82,4 +79,3 @@ def post_task():
 @blueprint.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error':'not found'}), 404)
-"""
