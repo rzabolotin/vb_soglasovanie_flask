d33fb41b862579928066b32924681c13ed3489e7
rzabolotin <rzabolotin@gmail.com>
2020-07-02 13:32:09 +1000

    Добавил администрирование файлов-вложений в админку

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,7 +8,7 @@ from webapp.api.views import blueprint as api_blueprint
 from webapp.filters import  register_my_jinja_filters
 from webapp.model import db, ma
 from webapp.soglasovanie.views import blueprint as soglasovanie_blueprint
-from webapp.soglasovanie.models import SoglasovanieTask, BusinessProcess
+from webapp.soglasovanie.models import SoglasovanieTask, BusinessProcess, FileAttachment
 from webapp.user.models import User
 from webapp.user.views import blueprint as user_blueprint
 
@@ -18,6 +18,7 @@ def create_app():
     
     db.init_app(app)
     migrate = Migrate(app, db)
+
     ma.init_app(app)
 
     login_manager = LoginManager()
@@ -33,6 +34,7 @@ def create_app():
     admin.add_view(MyUserView(User, db.session, name="Пользователи", endpoint="user_"))
     admin.add_view(MyModelView(BusinessProcess, db.session, name="Бизнес-процессы", category="Задачи"))
     admin.add_view(MyModelView(SoglasovanieTask, db.session, name="Задачи", category="Задачи"))
+    admin.add_view(MyModelView(FileAttachment, db.session, name="Задачи", category="Задачи"))
 
     register_my_jinja_filters(app)
 
