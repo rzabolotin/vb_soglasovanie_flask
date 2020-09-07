25f79d2a54788ef9d9799ea6dd223185a7bc0914
rzabolotin <rzabolotin@gmail.com>
2020-09-07 12:02:59 +1000

    Сделал редиректы на предыдущую страницу, после авторизации. А после выхода - редирект на авторизацию

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, current_user
 
 from webapp.user.forms import LoginForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 
 blueprint = Blueprint('user', __name__, url_prefix='/users/')
@@ -13,11 +14,11 @@ def login():
     """Форма авторизации"""
 
     if current_user.is_authenticated:
-        return redirect(url_for('soglasovanie.index', task_filter='active'))
+        return redirect(get_redirect_target())
 
     title = "Авторизация"
     login_form = LoginForm()
-    return render_template('user/login.html', page_title=title, form = login_form)
+    return render_template('user/login.html', page_title=title, form=login_form)
 
 
 @blueprint.route('/logout')
@@ -26,7 +27,7 @@ def logout():
 
     logout_user()
     flash('Вы успешно разлогинились')
-    return redirect(url_for('soglasovanie.index', task_filter='active'))
+    return redirect(url_for('user.login'))
 
 
 @blueprint.route('/process-login', methods=['POST'])
@@ -39,7 +40,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('soglasovanie.index', task_filter='active'))
+            return redirect(get_redirect_target())
 
     flash("Неправильные имя или пароль")
     return redirect(url_for('user.login'))
---
+++ webapp/utils.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse, urljoin
+from flask import request, url_for
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+def get_redirect_target():
+    """Определяем есть ли параметр next, или предыдущая страница referrer. и переходим на них если они безопасные
+    и не равные текущей странице, чтобы не зацикливаться на одной строанице"""
+    for target in [
+                request.args.get('next'),
+                request.referrer,
+                url_for('soglasovanie.index', task_filter='active')
+                ]:
+        if not target or target == request.base_url:
+            continue
+        if is_safe_url(target):
+            return target
