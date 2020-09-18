15a9667be640e92418f6e15a2902d2a18641d8b0
rzabolotin <rzabolotin@gmail.com>
2020-09-18 16:25:02 +1000

    сделал проверку что у пользователя заполнен пароль перед проверкой, а то ошибка была при вычислении хеша сделал задержку в 3 секунды, после 3-х неудачных попытки авторизоваться

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from time import sleep
+
+from flask import Blueprint, render_template, flash, redirect, url_for, session
 from flask_login import login_user, logout_user, current_user
 
 from webapp.user.forms import LoginForm
@@ -30,17 +32,33 @@ def logout():
     return redirect(url_for('user.login'))
 
 
+def make_sleep_if_too_many_login_attempts():
+    number_login_attempt = session.get('number_login_attempt', 0)
+
+    number_login_attempt += 1
+    number_login_attempt %= 10
+
+    session['number_login_attempt'] = number_login_attempt
+
+    print(number_login_attempt)
+
+    if number_login_attempt > 3:
+        sleep(3)
+
+
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
     """Обработка формы авторизации"""
+    make_sleep_if_too_many_login_attempts()
 
     form = LoginForm()
     user_name_lower = form.user_name.data.lower()
     if form.validate_on_submit():
         user = User.query.filter(User.user_name == user_name_lower).first()
-        if user and user.check_password(form.password.data):
+        if user and user.password and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me)
             flash('Вы успешно вошли на сайт')
+            session['number_login_attempt'] = 0
             return redirect(get_redirect_target())
 
     flash("Неправильные имя или пароль")
