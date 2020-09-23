19652eca046abac494450527cb6c106710ee66ec
rzabolotin <rzabolotin@gmail.com>
2020-09-23 10:05:21 +1000

    Сделал выделение новых бизнес процессов в списке желтым цветом

--- webapp/filters.py
+++ webapp/filters.py
@@ -1,19 +1,27 @@
 import pytz
+from datetime import datetime, timedelta
 
 
 def register_my_jinja_filters(app):
     @app.template_filter('datetime_vl')
-    def datetime_vl(value):
+    def datetime_vl(value, fmt=None):
         """ Фильтр даты для фласка, преобразует дату UTC в временную зону Владивостока"""
 
         if not value:
             return value
 
-        DATE_FORMAT = '%d.%m.%Y %H:%M'
+        if not fmt:
+            fmt = '%d.%m.%Y %H:%M'
 
         tz_utc = pytz.utc
         tz_vl = pytz.timezone('Asia/Vladivostok')
         value = tz_utc.localize(value, is_dst=None)
         local_dt = value.astimezone(tz_vl)
 
-        return local_dt.strftime(DATE_FORMAT)
+        return local_dt.strftime(fmt)
+
+    @app.template_filter('is_earlier_than_n_days')
+    def is_earlier_than_n_days(date, n_days):
+        if not date:
+            return False
+        return (datetime.now() - date) < timedelta(days=n_days)
