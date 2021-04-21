4e5f709ced57448d595585cc8d264137e29d1460
rzabolotin <rzabolotin@gmail.com>
2021-04-22 07:50:28 +1000

    Небольшие исправления по тестированию Трансгаз

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -7,4 +7,4 @@ class LoginForm(FlaskForm):
     user_name = StringField('Имя пользователя', validators=[DataRequired()], render_kw={'class': 'form-control'})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class': 'form-control'})
     remember_me = BooleanField("Запомнить меня", default=True, render_kw={'class': 'form-check-input'})
-    submit = SubmitField('Отправить', render_kw={'class': 'btn btn-primary'})
+    submit = SubmitField('Войти', render_kw={'class': 'btn btn-primary'})
