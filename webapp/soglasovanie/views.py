39d2715c22a4e54a2e59735c894181519cb20e09
Roman Zabolotin <rzabolotin@gmail.com>
2024-02-27 07:38:05 +1000

    feat: updates for BP Zakupki

--- webapp/soglasovanie/views.py
+++ webapp/soglasovanie/views.py
@@ -73,7 +73,7 @@ def show_task(task_id: str):
         (FileAttachment.bp_id == task.bp_id) & (FileAttachment.file_type == "ВложениеБизнесПроцесса")
     )
     partner_files = FileAttachment.query.filter(
-        (FileAttachment.bp_id == task.bp_id) & (FileAttachment.file_type == "УставнойДокумент")
+        (FileAttachment.bp_id == task.bp_id) & ((FileAttachment.file_type  == "УставнойДокумент") | (FileAttachment.file_type == "ФайлПобедителя"))
     )
     bp_reports = FileAttachment.query.filter(
         (FileAttachment.bp_id == task.bp_id) & (FileAttachment.file_type == "Отчет")
